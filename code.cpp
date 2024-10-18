b4b34f12f04477cbe85f0a3d777bb36d45c10fc9
NguyenThanhTruong030603 <145515640+NguyenThanhTruong030603@users.noreply.github.com>
2024-10-18 18:24:38 +0700

    first commit

---
+++ code.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &a);
+    printf("Nhap so thu hai: ");
+    scanf("%d", &b);
+
+
+    sum = a + b;
+
+   
+    printf("Tong cua %d và %d là: %d\n", a, b, sum);
+
+    return 0;
+}
+
